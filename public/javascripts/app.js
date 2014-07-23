ac8f085e3872c69d47dcf8ef27d17dff9135d5bb
Chris <cjolinger7@gmail.com>
2014-07-23 06:14:57 +0000

    Converted Bloc Jams to a single page application

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -266,14 +266,20 @@ if (document.URL.match(/\/album.html/)) {
 //require('./album');
 //require("./profile");
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
-  $scope.subText1 = "Bloc Jams";
-  $scope.subText = "Turn the music up!";
-
-  $scope.subText1Clicked = function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
+blocJams = angular.module('BlocJams', ['ui.router']);
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/landing.html'
+   });
+ }]);
+ 
+ // This is a cleaner way to call the controller than crowding it on the module definition.
+ blocJams.controller('Landing.controller', ['$scope', function($scope) {
+   $scope.subText = "Turn the music up!";
 
   $scope.subTextClicked = function() {
     $scope.subText += '!';
